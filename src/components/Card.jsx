35cdd4384fc60069eff3113b3c803b97068cc121
Arpit Sachan <sachanarpitknp@gmail.com>
2022-01-29 17:56:09 +0530

    all completd

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+export const Card = () => {
+  return (
+    <>
+      <li
+        className="card-item"
+        style={{
+          backgroundImage:
+            "url(" +
+            "https://irp-cdn.multiscreensite.com/d8037e1a/dms3rep/multi/Functional+Medicine+Doctor+Chicago.jpeg" +
+            ")",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div>
+          <h3 className="heading">Panel Discussion on Drug resistant TB</h3>
+
+          <div className="hello">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+          </div>
+        </div>
+      </li>
+    </>
+  );
+};
---
+++ src/components/Main.jsx
@@ -0,0 +1,31 @@
+import { Card } from "./Card";
+import "./Main.css";
+
+export const MainList = () => {
+  return (
+    <div>
+      <ul class="card-list">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </ul>
+    </div>
+  );
+};
